7991ae6f7a521c6629c2c71a2ea0f689af45b9b5
ricardofares <ricardoxdd28@outlook.com>
2023-03-29 07:36:45 -0300

    `schedule_event` function has been added.
      * The `schedule_event` inserts an event to be
        processed by a specified service at the specified
        time.
    
        Further, the event type is specified to indicate
        how the event will be handled.
    
      * For the time being, the `schedule_event is only
        supporting the ROOT-Sim (underlying optimistic
        simulator).
    
      This function is part of the iSPD's Engine Interface.

--- include/engine.h
+++ include/engine.h
@@ -1,3 +1,35 @@
 #ifndef ENGINE_H
 #define ENGINE_H
+
+///
+/// Backing Simulator
+///     0 - ROOT-Sim
+///
+#define SIM 0
+
+#if SIM == 0
+
+///
+/// ROOT-Sim
+///
+#include <ROOT-Sim.h>
+typedef lp_id_t   sid_t;
+typedef simtime_t timestamp_t;
+
+#endif
+
+/**
+ * @brief It schedules a event to be processed by the
+ *        specified service at that time.
+ *
+ *        Further, an event type is specified to indicate
+ *        how the event will be processed.
+ *
+ * @param service    the service
+ * @param time       the time
+ * @param event_type the event type
+ * @param event      the event
+ */
+void schedule_event(sid_t service, timestamp_t time, unsigned event_type, void *event);
+
 #endif // ENGINE_H
--- src/engine.c
+++ src/engine.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <engine.h>
 
+/**
+ * @brief It schedules a event to be processed by the
+ *        specified service at that time.
+ *
+ *        Further, an event type is specified to indicate
+ *        how the event will be processed.
+ *
+ * @param service    the service
+ * @param time       the time
+ * @param event_type the event type
+ * @param event      the event
+ */
+void schedule_event(sid_t service, timestamp_t time, unsigned event_type, void *event) {
+#if SIM == 0 /* ROOT-Sim */
+    ScheduleNewEvent(service, time, event_type, event, sizeof(*event));
+#endif
+}
+
 int main(int argc, char **argv) {
   printf("Engine\n");
   return 0;
