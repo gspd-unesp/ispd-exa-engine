a14b7e607a9ca36493a31758a3fbc35f7fec6d8e
ricardofares <ricardoxdd28@outlook.com>
2023-03-30 09:10:25 -0300

    Event size specification in `schedule_event`.

--- include/engine.h
+++ include/engine.h
@@ -20,18 +20,38 @@ typedef simtime_t timestamp_t;
 
 #endif
 
+struct task {
+    /*
+     * Task's processing size in mega-flops (MFlops).
+     */
+    double proc_size;
+
+    /*
+     * Task's communication size in megabits (Mb).
+     */
+    double comm_size;
+};
+
+struct event {
+    struct task *task;
+};
+
 /**
  * @brief It schedules a event to be processed by the
  *        specified service at that time.
  *
- *        Further, an event type is specified to indicate
- *        how the event will be processed.
+ *        An event type is specified to indicate which
+ *        event handler should process this event.
+ *
+ *        Further, in addition to the event, the event
+ *        size should be specified.
  *
  * @param service    the service
  * @param time       the time
  * @param event_type the event type
  * @param event      the event
+ * @param event_size the event size
  */
-ENGINE_INLINE void schedule_event(sid_t service, timestamp_t time, unsigned event_type, void *event);
+void schedule_event(sid_t service, timestamp_t time, unsigned event_type, void *event, size_t event_size);
 
 #endif // ENGINE_H
--- src/engine.c
+++ src/engine.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
 #include <engine.h>
 
-ENGINE_INLINE void schedule_event(sid_t service, timestamp_t time, unsigned event_type, void *event) {
+ENGINE_INLINE void schedule_event(sid_t service, timestamp_t time, unsigned event_type, void *event, size_t event_size) {
 #if SIM == 0 /* ROOT-Sim */
-    ScheduleNewEvent(service, time, event_type, event, sizeof(*event));
+    ScheduleNewEvent(service, time, event_type, event, event_size);
 #endif
-}
-
-int main(int argc, char **argv) {
-  printf("Engine\n");
-  return 0;
 }
