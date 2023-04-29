db787889305978cf9550e5edc14b1bdc892c20a8
ricardofares <ricardoxdd28@outlook.com>
2023-04-29 13:54:02 -0300

    Added a `vector` that keeps track of which links are connected to the `master`.
      * With that, the access to the `Scheduler`
        could be removed. such that the addition
        of the master's slaves are made transparently.

--- include/service/master.hpp
+++ include/service/master.hpp
@@ -1,6 +1,7 @@
 #ifndef ENGINE_MASTER_HPP
 #define ENGINE_MASTER_HPP
 
+#include <algorithm>
 #include <scheduler/scheduler.hpp>
 #include <service/service.hpp>
 #include <vector>
@@ -23,17 +24,34 @@ public:
     void onTaskArrival(timestamp_t time, const Task *t) override;
 
     /**
-     * Returns the scheduler.
+     * @brief It adds a link to this master.
      *
-     * @return the scheduler
+     * @param linkId the link identifier
      */
-    Scheduler<sid_t> *getScheduler()
+    void addLink(const sid_t linkId)
     {
-        return m_Scheduler;
+        // It checks if the link with the specified id has already been added to the vector.
+        if (std::find(m_Links.cbegin(), m_Links.cend(), linkId) != m_Links.cend())
+            die("Link %lu has already been added to the master %lu.", linkId, Service::getId());
+
+        // Add the link identifier.
+        m_Links.push_back(linkId);
+
+        // Add a resource branch.
+        m_Scheduler->addResource(linkId);
     }
 
 private:
     Scheduler<sid_t> *m_Scheduler;
+
+    /**
+     * @brief It represents a vector containing the link's identifiers
+     *        which are connected with this master.
+     *
+     *        Viewing it as a graph, the link represents the edge connecting
+     *        two nodes, being one of them this master.
+     */
+    std::vector<sid_t> m_Links;
 };
 
 #endif // ENGINE_MASTER_HPP
