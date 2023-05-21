16e0fcf61e4b55b94711d5c42d15ff57678e243a
ricardofares <ricardoxdd28@outlook.com>
2023-05-20 23:13:02 -0300

    Added a detailed documentation of the RouteDescriptor.

--- include/routing/route.hpp
+++ include/routing/route.hpp
@@ -1,25 +1,116 @@
 #ifndef ENGINE_ROUTING_ROUTE_HPP
 #define ENGINE_ROUTING_ROUTE_HPP
 
+#include <core/core.hpp>
 #include <cstdint>
 
-struct RouteDescriptor
+/**
+ * @brief A route descriptor.
+ *
+ * @details
+ *        The route descriptor is a piece of information of the events
+ *        that is used to route packets through a path between the source
+ *        and destination services.
+ *
+ *        The route descriptor stores the following informations.
+ *
+ *          `src`: This information is the source service's identifier of
+ *                 the route. It is used in conjunction with the destination
+ *                 service's identifier for index a routing table that contains
+ *                 the path between these two services.
+ *
+ *          `dest`: This information is the destination service's identifier of
+ *                  the route. It is used in conjunction with the source
+ *                  service's  identifier for index a routing table that
+ *                  contains the path between these two services.
+ *
+ *          `offset: Once we have the route between these two services, this
+ *                   information is used to identify which service in the route
+ *                   is the next to receive the packet.
+ */
+class RouteDescriptor
 {
+public:
     /**
-     * @brief A route descriptor contains the essential information
-     *        to describe the fastest path from a source vertex to
-     *        a destination vertex.
+     * @brief Construct a route descriptor specifying the source service,
+     *        the destination service and the offset for indexing the next
+     *        service in the route between the source and destination services.
      *
-     *        Further, this descriptor it is composed by three informations
-     *        that are the source service's identifier, the destination
-     *        service's identifier and the route's offset.
-     *
-     *                                                   ROUTE OFFSET
-     *           SOURCE VERTEX ID        DEST VERTEX ID
-     *        63                  34 33                    4 3  0
-     *        000000000...000000000  0000000000...0000000000 0000
+     * @param src the source service identifier
+     * @param dest the destination service identifier
+     * @param offset an offset used for indexing the next service's identifier
+     *               in the route between the source and destination services
      */
-    uint64_t m_Desc;
+    explicit RouteDescriptor(const uint64_t    src,
+                             const uint64_t    dest,
+                             const std::size_t offset)
+    {}
+
+    /**
+     * @brief Returns the route source service's identifier.
+     *
+     * @return the route source service's identifier
+     */
+    ENGINE_INLINE uint64_t getSource() const
+    {
+        return m_Src;
+    }
+
+    /**
+     * @brief Returns the route destination service's identifier.
+     *
+     * @return the route destination service's identifier
+     */
+    ENGINE_INLINE uint64_t getDest() const
+    {
+        return m_Dest;
+    }
+
+    /**
+     * @brief Returns the route offset that is used for indexing
+     *        the next service's identifier in the route.
+     *
+     * @return the route offset used that is for indexing the
+     *         next service's identifier in the route
+     */
+    ENGINE_INLINE std::size_t getOffset() const
+    {
+        return m_Offset;
+    }
+
+private:
+    /**
+     * @brief The route source service's identifier.
+     */
+    uint64_t m_Src;
+
+    /**
+     * @brief The route destination service's identifier.
+     */
+    uint64_t m_Dest;
+
+    /**
+     * @brief The route offset.
+     *
+     * @details
+     *        The route offset is used to index the next service's
+     *        identifier in a route.
+     *
+     *        For instance, suppose we have the route containing the
+     *        following services identifiers.
+     *
+     *          (l1, l2, l3, l4, l5)
+     *
+     *        Therefore, if the route offset is equal to 2, then the
+     *        next element to forward the packet is the service which
+     *        has the identifier `l3`.
+     *
+     *        Note that neither `l1` nor `l5` is the source or the
+     *        destination service, respectively. For more information
+     *        in how the route is represented, look at the documentation
+     *        provided by the `Route` and `RoutingTable` classes.
+     */
+    std::size_t m_Offset;
 };
 
 #endif // ENGINE_ROUTING_ROUTE_HPP
